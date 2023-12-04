b0c88e19c8e5330b5b1ef5e793751e8298c08858
bilal el haddadi <bilal.el.haddadi77@gmail.com>
2023-12-04 19:15:42 +0100

    add post

--- src/snappies/urls.py
+++ src/snappies/urls.py
@@ -3,5 +3,6 @@ from snappies import views
 urlpatterns = [
     path('getOne/<commande_id>', views.get_commande, name="get_commande"),
     path('getAll', views.get_commandes, name="get_commandes"),
+    path('create_commande', views.create_commande, name='create_commande'),
 
 ]
--- src/snappies/views.py
+++ src/snappies/views.py
@@ -2,6 +2,7 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Commande
+from django.views.decorators.csrf import csrf_exempt
 
 def get_commande(request, commande_id):
     if request.method == 'GET':
@@ -18,3 +19,17 @@ def get_commandes(request):
         commandes = Commande.objects.all()
         commandes_data = [{'id': commande.id, 'value': commande.value} for commande in commandes]
         return HttpResponse(json.dumps(commandes_data), content_type='application/json')
+    
+@csrf_exempt
+def create_commande(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        id = data.get('id')
+        value = data.get('value')
+        commande = Commande(id=id, value=value)
+        commandes_data = {'id': commande.id, 'value': commande.value}
+
+        commande.save()
+        return HttpResponse(json.dumps(commandes_data))
+    else:
+        return HttpResponse('error')
